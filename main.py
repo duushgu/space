ecd55864a0ed8827c22599313293d1e7898a3316
dush <96347847+duushgu@users.noreply.github.com>
2025-12-03 15:54:14 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,154 @@
+@namespace
+class SpriteKind:
+    Gas = SpriteKind.create()
+
+def on_a_pressed():
+    global darts, projectile
+    darts = [assets.image("""
+            Dart1
+            """),
+        assets.image("""
+            Dart2
+            """),
+        assets.image("""
+            Dart1
+            """)]
+    projectile = sprites.create_projectile_from_sprite(darts._pick_random(), mySprite, 0, -70)
+    projectile.start_effect(effects.warm_radial, 100)
+    music.play(music.create_sound_effect(WaveShape.SQUARE,
+            1600,
+            1,
+            255,
+            0,
+            300,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.CURVE),
+        music.PlaybackMode.UNTIL_DONE)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_on_overlap(sprite, otherSprite):
+    global enemySpeed
+    sprite.destroy(effects.confetti, 200)
+    otherSprite.destroy(effects.confetti, 200)
+    info.change_score_by(1)
+    if info.score() == 10:
+        info.change_score_by(5)
+        mySprite.say_text("+5 Level-Up Bonus!", 2000, False)
+        music.play(music.string_playable("C E G C G E G C ", 273),
+            music.PlaybackMode.UNTIL_DONE)
+        enemySpeed += 70
+    music.play(music.create_sound_effect(WaveShape.NOISE,
+            5000,
+            600,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.WARBLE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.projectile, on_on_overlap)
+
+def on_on_overlap2(sprite3, otherSprite3):
+    info.change_life_by(-1)
+    otherSprite3.destroy(effects.confetti, 400)
+    scene.camera_shake(4, 500)
+    music.play(music.create_sound_effect(WaveShape.SQUARE,
+            5000,
+            2801,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.VIBRATO,
+            InterpolationCurve.LOGARITHMIC),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+def on_on_overlap3(sprite2, otherSprite2):
+    statusbar.value = 100
+    otherSprite2.destroy()
+    music.play(music.create_sound_effect(WaveShape.SINE,
+            2889,
+            200,
+            255,
+            0,
+            200,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.player, SpriteKind.Gas, on_on_overlap3)
+
+def on_on_zero(status):
+    game.over(False)
+    music.play(music.string_playable("E D C A G E D C ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+statusbars.on_zero(StatusBarKind.energy, on_on_zero)
+
+my_enemy: Sprite = None
+myFuel: Sprite = None
+enemySpeed = 0
+projectile: Sprite = None
+darts: List[Image] = []
+statusbar: StatusBarSprite = None
+mySprite: Sprite = None
+scene.set_background_image(assets.image("""
+    Galaxy
+    """))
+scroller.scroll_background_with_speed(0, 10)
+mySprite = sprites.create(assets.image("""
+    Rocket
+    """), SpriteKind.player)
+controller.move_sprite(mySprite)
+mySprite.set_stay_in_screen(True)
+animation.run_image_animation(mySprite,
+    assets.animation("""
+        Flying Rocket
+        """),
+    100,
+    True)
+statusbar = statusbars.create(20, 4, StatusBarKind.energy)
+statusbar.attach_to_sprite(mySprite, -25, 0)
+music.play(music.string_playable("C G F E D C D C ", 273),
+    music.PlaybackMode.UNTIL_DONE)
+
+def on_update_interval():
+    global myFuel
+    myFuel = sprites.create_projectile_from_side(assets.image("""
+        Fuel
+        """), 0, 40)
+    myFuel.x = randint(5, 155)
+    myFuel.set_kind(SpriteKind.Gas)
+game.on_update_interval(5000, on_update_interval)
+
+def on_update_interval2():
+    global my_enemy
+    my_enemy = sprites.create_projectile_from_side(assets.image("""
+        Spider
+        """), 0, 50)
+    my_enemy.x = randint(5, 150)
+    my_enemy.set_kind(SpriteKind.enemy)
+    animation.run_image_animation(my_enemy,
+        assets.animation("""
+            Flying Spider
+            """),
+        100,
+        True)
+game.on_update_interval(2000, on_update_interval2)
+
+def on_update_interval3():
+    statusbar.value += -1
+game.on_update_interval(300, on_update_interval3)
+
+def on_update_interval4():
+    global my_enemy
+    my_enemy = sprites.create_projectile_from_side(assets.image("""
+        Spider
+        """), 0, 50)
+    my_enemy.x = randint(5, 150)
+    my_enemy.set_kind(SpriteKind.enemy)
+    animation.run_image_animation(my_enemy,
+        assets.animation("""
+            Flying Spider
+            """),
+        100,
+        True)
+game.on_update_interval(1200, on_update_interval4)
---
+++ main.ts
@@ -0,0 +1,92 @@
+namespace SpriteKind {
+    export const Gas = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    darts = [assets.image`Dart1`, assets.image`Dart2`, assets.image`Dart1`]
+    projectile = sprites.createProjectileFromSprite(darts._pickRandom(), mySprite, 0, -70)
+    projectile.startEffect(effects.warmRadial, 100)
+    music.play(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy(effects.confetti, 200)
+    otherSprite.destroy(effects.confetti, 200)
+    info.changeScoreBy(1)
+    if (info.score() == 1) {
+        info.changeScoreBy(5)
+        mySprite.sayText("easy?, now INTENSIVE", 3000, false)
+        music.play(music.stringPlayable("C E G C G E G C ", 273), music.PlaybackMode.UntilDone)
+        enemySpeed = 99
+    }
+    music.play(music.createSoundEffect(WaveShape.Noise, 5000, 600, 255, 0, 500, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite3, otherSprite3) {
+    info.changeLifeBy(-1)
+    otherSprite3.destroy(effects.confetti, 400)
+    scene.cameraShake(4, 500)
+    music.play(music.createSoundEffect(WaveShape.Square, 5000, 2845, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gas, function (sprite2, otherSprite2) {
+    statusbar.value = 100
+    otherSprite2.destroy()
+    music.play(music.createSoundEffect(WaveShape.Sine, 2889, 200, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+info.onLifeZero(function () {
+    game.over(false)
+    music.play(music.stringPlayable("E D C A G E D C ", 273), music.PlaybackMode.UntilDone)
+})
+statusbars.onZero(StatusBarKind.Energy, function (status) {
+    game.over(false)
+    music.play(music.stringPlayable("E D C A G E D C ", 273), music.PlaybackMode.UntilDone)
+})
+let my_enemy: Sprite = null
+let myFuel: Sprite = null
+let projectile: Sprite = null
+let darts: Image[] = []
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+let enemySpeed = 0
+enemySpeed = 50
+scene.setBackgroundImage(assets.image`Galaxy`)
+scroller.scrollBackgroundWithSpeed(0, 10)
+mySprite = sprites.create(assets.image`Rocket`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Flying Rocket`,
+100,
+true
+)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 0)
+music.play(music.stringPlayable("C G F E D C D C ", 246), music.PlaybackMode.UntilDone)
+game.onUpdateInterval(5000, function () {
+    myFuel = sprites.createProjectileFromSide(assets.image`Fuel`, 0, 40)
+    myFuel.x = randint(5, 155)
+    myFuel.setKind(SpriteKind.Gas)
+})
+game.onUpdateInterval(2000, function () {
+    my_enemy = sprites.createProjectileFromSide(assets.image`Spider`, 0, 50)
+    my_enemy.x = randint(5, 150)
+    my_enemy.setKind(SpriteKind.Enemy)
+    animation.runImageAnimation(
+    my_enemy,
+    assets.animation`Flying Spider`,
+    100,
+    true
+    )
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -1
+})
+game.onUpdateInterval(1200, function () {
+    my_enemy = sprites.createProjectileFromSide(assets.image`Spider`, 0, 50)
+    my_enemy.x = randint(5, 150)
+    my_enemy.setKind(SpriteKind.Enemy)
+    animation.runImageAnimation(
+    my_enemy,
+    assets.animation`Flying Spider`,
+    100,
+    true
+    )
+})
